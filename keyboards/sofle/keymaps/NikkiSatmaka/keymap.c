ee905df5adc9fca364128c8fab61e3b7891a9d78
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-07-11 17:28:30 +0700

    fix old keycodes

--- keyboards/sofle/keymaps/NikkiSatmaka/keymap.c
+++ keyboards/sofle/keymaps/NikkiSatmaka/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                        KC_J,    KC_L,  KC_U,     KC_Y,    KC_QUOT,  KC_BSPC,
   KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                        KC_M,    KC_N,  KC_E,     KC_I,    KC_O,     KC_SCLN,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,      XXXXXXX, KC_K,    KC_H,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
-                  KC_LGUI, KC_LALT, KC_LCTRL, L_EXT, KC_LSFT,       KC_SPC, L_SYM, KC_RCTRL, KC_RALT, KC_RGUI
+                  KC_LGUI, KC_LALT, KC_LCTL, L_EXT, KC_LSFT,       KC_SPC, L_SYM, KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_BSPC,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX, KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
-                  KC_LGUI, KC_LALT, KC_LCTRL, L_EXT, KC_LSFT,      KC_SPC, L_SYM, KC_RCTRL, KC_RALT, KC_RGUI
+                  KC_LGUI, KC_LALT, KC_LCTL, L_EXT, KC_LSFT,      KC_SPC, L_SYM, KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /* EXT
@@ -500,7 +500,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_PGDN);
         } else {
             tap_code(KC_PGUP);
         }
