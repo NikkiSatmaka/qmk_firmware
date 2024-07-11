00acfb18c118689c90cb9cf39448011d2b409b53
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-07-11 16:50:17 +0700

    update copyright 2022 to 2024

--- keyboards/sofle/keymaps/NikkiSatmaka/keymap.c
+++ keyboards/sofle/keymaps/NikkiSatmaka/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 NikkiSatmaka
+/* Copyright 2024 NikkiSatmaka
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
